cf223b9059fcc4f38a49319d6c77bf1b3a5a5196
CharmGuarde <charmchristal.guarde@ontariotechu.net>
2025-11-03 17:15:27 -0500

    making a template

--- express-portfolio/routes/index.js
+++ express-portfolio/routes/index.js
@@ -1,9 +1,36 @@
+/*
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+*/
+
+var express = require('express');
+var router = express.Router();
+
+/* Home */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* About */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* Projects */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* Contact */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
