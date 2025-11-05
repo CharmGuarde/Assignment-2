deb284e88cc6a09b48634afa1b5b560428bd6a7f
CharmGuarde <charmchristal.guarde@ontariotechu.net>
2025-11-04 21:05:21 -0500

    fixing layout

--- express-portfolio/app.js
+++ express-portfolio/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layout'); // this tells EJS to use layout.ejs as the base
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,18 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- express-portfolio/routes/index.js
+++ express-portfolio/routes/index.js
@@ -1,34 +1,22 @@
-/*
 var express = require('express');
 var router = express.Router();
 
-GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-*/
-
-var express = require('express');
-var router = express.Router();
-
-/* Home */
+/* HOME PAGE */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* About */
+/* ABOUT PAGE */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About Me' });
 });
 
-/* Projects */
+/* PROJECTS PAGE */
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
-/* Contact */
+/* CONTACT PAGE */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
